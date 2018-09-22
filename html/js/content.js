15aff6f267bd0700f54bee662a145b992b5cfc4d
jschroeter <j.schroeter@devwerks.net>
2018-09-22 22:48:28 +0200

    added events

--- html/js/content.js
+++ html/js/content.js
@@ -16,5 +16,15 @@
                 $('#content li').wrap('<ul/>');
             }
         });
+        $.ajax({ 
+            url: 'https://raw.githubusercontent.com/devwerks/XSS-Cheatsheet/master/events.json', 
+            dataType: 'json',
+            success: function (data) { 
+                $.each(data, function(index, element) {
+                    $('#events').append('<li><div class="item"><a id="event' + element.id + '"></a><a href="#event' + element.id + '">#' + element.id + '</a> <a target="_blank" href="editor.html#event' + element.id + '">Editor</a><code class="data">' + sanitize(element.data) + '</code></div></li>');
+                });
+                $('#events li').wrap('<ul/>');
+            }
+        });
     });
 })();
