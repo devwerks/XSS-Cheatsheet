eff8a59733c22e3da8394310d0e24fe2732019cc
jschroeter <j.schroeter@devwerks.net>
2019-10-10 21:31:48 +0200

    added copy to clipboard

--- html/js/content.js
+++ html/js/content.js
@@ -11,7 +11,7 @@
             dataType: 'json',
             success: function (data) { 
                 $.each(data, function(index, element) {
-                    $('#content').append('<li><div class="item"><a id="' + element.id + '"></a><a href="#' + element.id + '">#' + element.id + '</a> <a target="_blank" href="editor.html#' + element.id + '">Editor</a><code class="data">' + sanitize(element.data) + '</code></div></li>');
+                    $('#content').append('<li><div class="item"><a id="' + element.id + '"></a><a href="#' + element.id + '">#' + element.id + '</a> <a target="_blank" href="editor.html#' + element.id + '">Editor</a> <a id="copy" href="#' + sanitize(element.data) + '" onclick="copy(this);">Copy</a><code class="data">' + sanitize(element.data) + '</code></div></li>');
                 });
                 $('#content li').wrap('<ul/>');
             }
@@ -21,10 +21,20 @@
             dataType: 'json',
             success: function (data) { 
                 $.each(data, function(index, element) {
-                    $('#events').append('<li><div class="item"><a id="event' + element.id + '"></a><a href="#event' + element.id + '">#' + element.id + '</a> <a target="_blank" href="editor.html#event' + element.id + '">Editor</a><code class="data">' + sanitize(element.data) + '</code></div></li>');
+                    $('#events').append('<li><div class="item"><a id="event' + element.id + '"></a><a href="#event' + element.id + '">#' + element.id + '</a> <a target="_blank" href="editor.html#event' + element.id + '">Editor</a> <a id="copy" href="#' + sanitize(element.data) + '" onclick="copy(this);">Copy</a><code class="data">' + sanitize(element.data) + '</code></div></li>');
                 });
                 $('#events li').wrap('<ul/>');
             }
         });
+        
     });
 })();
+
+function copy(obj) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = obj.getAttribute("href").slice(1);;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
